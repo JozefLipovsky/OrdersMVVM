d4aa67f5fabf909731785c5be3f6fd5c3834b51a
Jozef Lipovsky <j.lipovsky@me.com>
2016-08-21 15:34:33 +0200

    Add UsersViewModel

---
+++ OrdersMVVM/ViewModels/UsersViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  UsersViewModel.swift
+//  OrdersMVVM
+//
+//  Created by JoLi on 2016-08-21.
+//  Copyright © 2016 JoLi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class UsersViewModel {
+    private let realm : Realm
+    var users: Results<User>?
+    
+    init() {
+        self.realm = try! Realm()
+        self.users = realm.objects(User.self)
+    }
+    
+    
+    func refreshData(completion: () -> Void) {
+//        APIManager.downloadUsers { (users, error) in
+//            if let users = users {
+//                StorageManager.save(users)
+//            }
+//            
+//            if let downloadError = error {
+//                // return error in completion block for alert...
+//                print(downloadError)
+//            }
+//            
+//            completion()
+//        }
+    }
+    
+    
+    func numberOfUsers() -> Int {
+        return users!.count
+    }
+    
+    
+    func nameOfUser(at index: Int) -> String {
+        let user = users![index]
+        return user.name
+    }
+    
+    
+    func phoneOfUser(at index: Int) -> String {
+        let user = users![index]
+        return user.phone
+    }
+    
+    
+    func pictureURLOfUser(at index: Int) -> String {
+        let user = users![index]
+        return user.pictureURL
+    }
+}
+
