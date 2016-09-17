4cbac417280cc6f8db04c47bac05f1f64c8d78bf
Jozef Lipovsky <j.lipovsky@me.com>
2016-09-17 16:04:07 +0200

    Add OrderTableCell config

---
+++ OrdersMVVM/Views/OrderTableCell.swift
@@ -0,0 +1,33 @@
+//
+//  OrderTableCell.swift
+//  OrdersMVVM
+//
+//  Created by JoLi on 2016-09-17.
+//  Copyright © 2016 JoLi. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+
+    
+    func configure(name name: String, count: String) {
+        nameLabel.text = name
+        countLabel.text = count + "x"
+    }
+    
+    
+    private func setupUI() {
+        self.preservesSuperviewLayoutMargins = true;
+        self.contentView.preservesSuperviewLayoutMargins = true;
+        self.selectionStyle = .None
+    }
+}
