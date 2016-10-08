1c869517e7f750d0e8cfd26dde3791e07f824f38
Jozef Lipovsky <j.lipovsky@me.com>
2016-10-08 19:19:16 +0200

    Update ImageUtility to swift 3 syntax

--- OrdersMVVM/Networking/ImageUtility.swift
+++ OrdersMVVM/Networking/ImageUtility.swift
@@ -14,11 +14,11 @@ class ImageUtility: NSObject {
     private static let baseURL = NSURL(string: "https://inloop-contacts.appspot.com/")
     
     static func setImage(with pictureURL: String, onView imageView: UIImageView) {
-        let url = NSURL(string: pictureURL, relativeToURL: baseURL)
+        let url = URL(string: pictureURL, relativeTo: baseURL as URL?)
         let absoluteURL = url?.absoluteURL
 
         if let url = absoluteURL {
-            imageView.af_setImageWithURL(url, placeholderImage: UIImage.init(named: "Avatar_placeholder"), filter: AspectScaledToFillSizeFilter(size: imageView.frame.size),imageTransition: .CrossDissolve(0.1))
+            imageView.af_setImage(withURL: url, placeholderImage: UIImage.init(named: "Avatar_placeholder"), filter: AspectScaledToFillSizeFilter(size: imageView.frame.size), imageTransition: .crossDissolve(0.1))
         }
     }
 }
