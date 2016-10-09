e70cfcd595754fe9dc104757f1ebb2c009658bf6
Jozef Lipovsky <j.lipovsky@me.com>
2016-10-09 11:53:31 +0200

    Fix add user api call parameters encoding

--- OrdersMVVM/Networking/APIManager.swift
+++ OrdersMVVM/Networking/APIManager.swift
@@ -28,12 +28,11 @@ class APIManager: NSObject {
     }
     
     
-    
     static func addUser(withName name: String, phone: String, completion:@escaping (User?, Error?) -> Void) {
         let contactsURL = "\(APIManager.baseURL)contactendpoint/v1/contact"
         let parameters = ["name" : name, "phone" : phone]
         
-        Alamofire.request(contactsURL, method: .post, parameters: parameters, encoding: URLEncoding.httpBody).responseObject { (response: DataResponse<User>) in
+        Alamofire.request(contactsURL, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseObject { (response: DataResponse<User>) in
             switch response.result {
             case .success(let user):
                 completion(user, nil)
@@ -44,31 +43,6 @@ class APIManager: NSObject {
         }
     }
 
-    
-    
-//    static func addUser(withName name: String, phone: String, completion:(_ user: User?, _ error: Error?) -> Void) {
-//        let contactsURL = "\(APIManager.baseURL)contactendpoint/v1/contact"
-//        
-//        Alamofire.request(.POST, contactsURL, parameters: [:], encoding: .Custom({ (request, parameters) -> (NSMutableURLRequest, NSError?) in
-//            // name and phone need to be send within body, not as url params
-//            let userParameters = ["name" : name, "phone" : phone]
-//            let mutableRequest = request.URLRequest.copy() as! NSMutableURLRequest
-//            let data = try! NSJSONSerialization.dataWithJSONObject(userParameters, options: .PrettyPrinted)
-//            mutableRequest.HTTPBody = data
-//            mutableRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//            return (mutableRequest, nil)
-//            
-//        })).responseObject { (response: Response<User, NSError>) in
-//            switch response.result {
-//            case .Success(let user):
-//                completion(user: user, error: nil)
-//                
-//            case .Failure(let error):
-//                completion(user: nil, error: error)
-//            }
-//        }
-//    }
-    
     
     static func downloadOrders(forUserID id: String, completion: @escaping ([Order]?, Error?) -> Void) {
         let ordersURL = "\(APIManager.baseURL)orderendpoint/v1/order/" + id
