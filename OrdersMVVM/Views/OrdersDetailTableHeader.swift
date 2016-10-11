f5e2241827ba798e41d35fb9fc1f155a113ac8a4
Jozef Lipovsky <j.lipovsky@me.com>
2016-10-11 20:16:44 +0200

    Add OrdersDetailTableHeader

---
+++ OrdersMVVM/Views/OrdersDetailTableHeader.swift
@@ -0,0 +1,16 @@
+//
+//  OrdersDetailTableHeader.swift
+//  OrdersMVVM
+//
+//  Created by JoLi on 2016-10-11.
+//  Copyright © 2016 JoLi. All rights reserved.
+//
+
+import UIKit
+
+class OrdersDetailTableHeader: UITableViewHeaderFooterView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    
+}
